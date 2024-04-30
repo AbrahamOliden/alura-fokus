2a2411ab5f0e9d335a3d14cf71f8a2063d8e8a9c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-29 23:08:03 -0600

    countdown and startPause functions added as the basis for timer

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 const html = document.querySelector('html');
 const title = document.querySelector('.app__title');
 const image = document.querySelector('.app__image');
+const interExclusiveButtons = document.querySelectorAll('.app__card-button');
 const focusButton = document.querySelector('.app__card-button--enfoque');
 const shortRestButton = document.querySelector('.app__card-button--corto')
 const longRestButton = document.querySelector('.app__card-button--largo');
+const musicButton = document.querySelector('#alternar-musica');
+const musicAudio = new Audio('./sonidos/luna-rise-part-one.mp3');
+const startPauseButton = document.querySelector('#start-pause');
+
+let elapsedTime = 5;
+let interval = null;
+
+musicAudio.loop = true;
 
 focusButton.addEventListener('click', () => {
     changeContext('enfoque');
+    focusButton.classList.add('active');
 } );
 
 shortRestButton.addEventListener('click', () => {
     changeContext('descanso-corto');
+    shortRestButton.classList.add('active');
 } );
 
 longRestButton.addEventListener('click', () => {
     changeContext('descanso-largo');
+    longRestButton.classList.add('active');
+
+});
+
+musicButton.addEventListener('change', () => {
+    musicAudio.paused
+        ? musicAudio.play()
+        : musicAudio.pause();
 });
 
 function changeContext(context) {
@@ -43,4 +62,20 @@ function changeContext(context) {
         default:
             break;
     }
+
+    interExclusiveButtons.forEach((context) => {
+        context.classList.remove('active')
+    });
+};
+
+const countdown = () => {
+    startPause();
+    elapsedTime--;
+    console.log(elapsedTime);
+};
+
+startPauseButton.addEventListener('click', countdown);
+
+function startPause() {
+    interval = setInterval(countdown, 1000);
 };
