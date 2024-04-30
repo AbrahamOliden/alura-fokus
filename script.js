1554e22646f2191c35c7cbc30894f5987a67c780
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-29 19:04:02 -0600

    changeContext function modifies .app__title's inner HTML

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const html = document.querySelector('html');
+const title = document.querySelector('.app__title');
 const image = document.querySelector('.app__image');
 const focusButton = document.querySelector('.app__card-button--enfoque');
 const shortRestButton = document.querySelector('.app__card-button--corto')
@@ -19,4 +20,27 @@ longRestButton.addEventListener('click', () => {
 function changeContext(context) {
     html.setAttribute('data-contexto', context);
     image.setAttribute('src', `/imagenes/${context}.png`);
+
+    switch (context) {
+        case 'enfoque':
+            title.innerHTML = `
+            Optimiza tu productividad,<br>
+                <strong class="app__title-strong" >sumérgete en lo que importa.</strong>
+            `;
+            break;
+        case 'descanso-corto':
+            title.innerHTML = `
+            ¿Qué tal tomar un respiro?<br>
+                <strong class="app__title-strong" >¡Haz una pausa corta!</strong>
+            `;
+            break;
+        case `descanso-largo`:
+            title.innerHTML = `
+            Hora de volver a la superficie<br>
+                <strong class="app__title-strong" >Haz una pausa larga.</strong>
+            `;
+            break;
+        default:
+            break;
+    }
 };
